0b11fc3ef8f9b3225b31f933d62053bf66641057
Sanaz Soltani <74157401+Snzslt@users.noreply.github.com>
2024-03-19 22:21:54 -0400

    Adding Automatic Logout

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,20 +1,38 @@
-import { redirect } from "react-router-dom";
+import { redirect } from 'react-router-dom';
 
-
-export function getAuthToken(){
-    const token = localStorage.getItem('token');
-    return token;
+export function getTokenDuration() {
+  const storedExpirationDate = localStorage.getItem('expiration');
+  const expirationDate = new Date(storedExpirationDate);
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getTime();
+  return duration;
 }
-export function tokenLoader(){
-    return getAuthToken();
+
+export function getAuthToken() {
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+
+  if (tokenDuration < 0) {
+    return 'EXPIRED';
+  }
+
+  return token;
 }
 
+export function tokenLoader() {
+  const token = getAuthToken();
+  return token;
+}
 
-export function checkAuthLoader(){
-    const token = getAuthToken();
+export function checkAuthLoader() {
+  const token = getAuthToken();
 
-    if(!token){
-        return redirect('/auth');
-    }
-    return null; 
+  if (!token) {
+    return redirect('/auth');
+  }
 }
